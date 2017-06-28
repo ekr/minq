b5e1d36239269b3ae3262b2360e7480e1c48554d
EKR <ekr@rtfm.com>
2017-06-28 06:47:42 -0700

    Make client run indefinitely

--- bin/client/main.go
+++ bin/client/main.go
@@ -4,11 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	
-	"github.com/ekr/minq"	
+
+	"github.com/ekr/minq"
 )
 
 var addr string
+
+func readUDP(s *net.UDPConn) []byte {
+	b := make([]byte, 8192)
+
+	n, _, err := s.ReadFromUDP(b)
+	if err != nil {
+		fmt.Println("Error reading from UDP socket: ", err)
+		return nil
+	}
+
+	if n == len(b) {
+		fmt.Println("Underread from UDP socket")
+		return nil
+	}
+	b = b[:n]
+	return b
+}
 
 func main() {
 	flag.StringVar(&addr, "addr", "localhost:4433", "[host:port]")
@@ -26,33 +43,38 @@ func main() {
 	}
 
 	utrans := minq.NewUdpTransport(usock, uaddr)
-	
+
 	conn := minq.NewConnection(utrans, minq.RoleClient, minq.TlsConfig{})
 
 	// Start things off.
 	_, err = conn.CheckTimer()
 
 	for !conn.Established() {
-		b := make([]byte, 8192)
-		
-		n, _, err := usock.ReadFromUDP(b)
-		if err != nil {
-			fmt.Println("Error reading from UDP socket: ", err)
+		b := readUDP(usock)
+		if b == nil {
 			return
 		}
 
-		if n == len(b) {
-			fmt.Println("Underread from UDP socket")
-			return
-		}
-		b = b[:n]
-		
 		err = conn.Input(b)
 		if err != nil {
-			fmt.Println("Error in QUIC handshake: ", err)
+			fmt.Println("Error", err)
 			return
 		}
 	}
 
 	fmt.Println("Connection established")
+
+	for {
+		b := readUDP(usock)
+		if b == nil {
+			return
+		}
+
+		err = conn.Input(b)
+		if err != nil {
+			fmt.Println("Error post-handshake: ", err)
+			return
+		}
+
+	}
 }
