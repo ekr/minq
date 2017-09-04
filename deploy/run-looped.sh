133bbd2b7528b99190822995436923d7623a283e
EKR <ekr@rtfm.com>
2017-09-04 15:16:12 -0700

    Update docker config to expose logs

--- deploy/run-looped.sh
+++ deploy/run-looped.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+node deploy/logserver/server.js /tmp/minq.log &
 while true; do
     echo -n "Starting server as "
     echo ${SNAME}
-    /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME}
+    /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME} -log /tmp/minq.log
     echo "Server crashed"
 done
         
