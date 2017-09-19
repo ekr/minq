40997de5ad1b5f8ba9050e987f1e5a99f6f9a373
EKR <ekr@rtfm.com>
2017-09-18 20:56:50 -0700

    Make dockerized server run HTTP

--- deploy/run-looped.sh
+++ deploy/run-looped.sh
@@ -3,7 +3,7 @@ nodejs /go/src/github.com/ekr/minq/deploy/logserver/server.js /tmp/minq.log &
 while true; do
     echo -n "Starting server as "
     echo ${SNAME}
-    /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME} -log /tmp/minq.log
+    /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME} -log /tmp/minq.log -http
     echo "Server crashed"
 done
         
