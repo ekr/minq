f72c4d98c9bfb3849c73f1ca7df823b1c00ac851
EKR <ekr@rtfm.com>
2018-07-15 11:09:00 -0700

    Add some logging

--- deploy/run-looped.sh
+++ deploy/run-looped.sh
@@ -3,7 +3,7 @@ nodejs /go/src/github.com/ekr/minq/deploy/logserver/server.js /tmp/minq.log &
 while true; do
     echo -n "Starting server as "
     echo ${SNAME}
-    /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME} -log /tmp/minq.log -http -standalone
+    MINQ_LOG=connection,packet /go/bin/server -addr 0.0.0.0:4433 -server-name ${SNAME} -log /tmp/minq.log -http -standalone
     echo "Server crashed"
 done
         
