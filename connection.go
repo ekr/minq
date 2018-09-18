c2dc9779adf8f958ffeccbdf0fc7fb7c476c7c0b
EKR <ekr@rtfm.com>
2018-09-18 07:14:04 -0700

    Change version #

--- connection.go
+++ connection.go
@@ -56,7 +56,7 @@ const (
 type VersionNumber uint32
 
 const (
-	kQuicDraftVersion   = 13
+	kQuicDraftVersion   = 14
 	kQuicVersion        = VersionNumber(0xff000000 | kQuicDraftVersion)
 	kQuicGreaseVersion1 = VersionNumber(0x1a1a1a1a)
 	kQuicGreaseVersion2 = VersionNumber(0x2a2a2a2a)
